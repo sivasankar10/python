608ab74085cd8024a8fdb0bb33d4ffb073e1b273
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2022-12-28 11:27:31 +0530

    Create VOTE.py

---
+++ VOTE.py
@@ -0,0 +1,7 @@
+n = int(input("Enter your age : "))
+
+if n >= 18:
+    print("CONGRATULATIONS YOU ARE ELIGIBLE TO VOTE")
+
+else:
+    print( "SORRY YOU ARE NOT ELIGIBLE TO VOTE \n YOU HAVE" ,(18 - n),"YEARS LEFT TO VOTE")
